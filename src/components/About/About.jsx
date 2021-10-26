cf4f27153b82e33e5a8d568367fe5b02c93012e1
BrunoDevMoraes <brunodevmoraes@gmail.com>
2021-10-26 16:33:43 -0300

    Add about section

---
+++ src/components/About/About.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Bruno from '../../images/Bruno.jpg';
+import './About.css';
+
+export default function About() {
+  return (
+    <div id="about">
+      <img src={Bruno} alt="Bruno smiling "/>
+      <p>
+        I'm passionate about technology, innovation and project development. I am communicative, creative and I believe in iniciatives that really care about people and have the proposal of building solutions for an integrated society. On my way to become a full stack developer, I study Software Development at Trybe.
+      </p>
+    </div>
+  )
+}
--- src/components/Body/Body.jsx
+++ src/components/Body/Body.jsx
@@ -1,12 +1,11 @@
-import React from 'react'
+import React from 'react';
+import About from '../About/About';
 import './Body.css'
 
 export default function Body() {
   return (
     <div id="body">
-      <img src="https://images.unsplash.com/photo-1587831990711-23ca6441447b?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8ZGVza3RvcCUyMGNvbXB1dGVyfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&w=1000&q=80" alt=""/>
-      <img src="https://images.unsplash.com/photo-1587831990711-23ca6441447b?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8ZGVza3RvcCUyMGNvbXB1dGVyfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&w=1000&q=80" alt=""/>
-      <img src="https://images.unsplash.com/photo-1587831990711-23ca6441447b?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8ZGVza3RvcCUyMGNvbXB1dGVyfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&w=1000&q=80" alt=""/>
+      <About/>
     </div>
   )
 }
