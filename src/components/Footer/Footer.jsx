de219626f1d6dbf1bbb34559deb0606e51b77068
BrunoDevMoraes <brunodevmoraes@gmail.com>
2021-10-25 22:19:46 -0300

    Add footer icons

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,10 +1,25 @@
 import React from 'react'
 import './Footer.css';
+import githubLogo from '../../images/github.svg';
+import envelope from '../../images/envelope.svg';
+import linkedin from '../../images/linkedin.svg';
+import whattsapp from '../../images/whatsapp.svg';
 
 export default function Footer() {
   return (
     <div id="footer">
-      <h3>Footer</h3>
+      <a href="https://www.linkedin.com/in/brunodevmoraes/" target="blank">
+        <img src={linkedin} alt="Linkedin's logo" width="20vw"/>
+      </a>
+      <a href="https://github.com/BrunoDevMoraes" target="blank" >
+        <img src={githubLogo} alt="GitHub's logo" width="20vw"/>
+      </a>
+      <a href="https://mailto:brunodevmoraes@gmail.com" target="blank">
+        <img src={envelope} alt="Envelope" width="20vw"/>
+      </a>
+      <a href="https://api.whatsapp.com/send?phone=32991172902" target="blank">
+        <img src={whattsapp} alt="WhatsApp's logo" width="20vw"/>
+      </a>
     </div>
   )
 }
