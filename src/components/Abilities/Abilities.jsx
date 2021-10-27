5831de7d073d98490ce11144591d478f6158f8fc
BrunoDevMoraes <brunodevmoraes@gmail.com>
2021-10-27 17:45:46 -0300

    Add Abilities sectiion and fix header buttons bug

---
+++ src/components/Abilities/Abilities.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import './Abilities.css';
+
+export default function Abilities() {
+  return (
+    <div id="abilities">
+      <div id="hard">
+        <h3>Hard Skills</h3>
+        <ul>
+          <li>JavaScript</li>
+          <li>HTML5</li>
+          <li>CSS3</li>
+          <li>React JS</li>
+          <li>Redux</li>
+          <li>Context API</li>
+          <li>React Hooks</li>
+          <li>C</li>
+          <li>C++</li>
+          <li>Git</li>
+        </ul>
+      </div>
+      <div id="soft">
+        <h3>Soft Skills</h3>
+        <ul>
+          <li>Communication</li>
+          <li>Creativity</li>
+          <li>Empathy</li>
+          <li>Team Work</li>
+          <li>Negotiation</li>
+          <li>Critical Thinking</li>
+          <li>Leadership</li>
+        </ul>
+      </div>
+    </div>
+  )
+}
--- src/components/Body/Body.jsx
+++ src/components/Body/Body.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
+import Abilities from '../Abilities/Abilities';
 import About from '../About/About';
+import Projects from '../Projects/Projects';
 import './Body.css'
 
 export default function Body() {
   return (
     <div id="body">
       <About/>
+      <Abilities/>
+      <Projects/>
     </div>
   )
 }
