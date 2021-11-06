334f4562c008ef6a884ad43cd2235f82bf659c68
BrunoDevMoraes <brunodevmoraes@gmail.com>
2021-11-05 23:02:30 -0300

    Add light background page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import React from 'react'
-import { Route, Switch } from 'react-router-dom'
-import MainPage from './pages/MainPage/MainPage'
-import './App.css'
+import React, { useContext } from 'react';
+import { Route, Switch } from 'react-router-dom';
+import MainPage from './pages/MainPage/MainPage';
+import './App.css';
+import AppContext from './context/AppContext';
 
 export default function App() {
+  const { theme } = useContext(AppContext);
   return (
-    <div>
+    <div className={theme === 'light' ? 'light-app' : 'dark-app'}>
       <Switch>
         <Route path="/" component={ MainPage }/>
       </Switch>
--- src/pages/MainPage/MainPage.js
+++ src/pages/MainPage/MainPage.js
@@ -1,12 +1,14 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import Body from '../../components/Body/Body'
 import Footer from '../../components/Footer/Footer'
 import Header from '../../components/Header/Header'
+import AppContext from '../../context/AppContext'
 import './MainPage.css'
 
 export default function MainPage() {
+  const { theme } = useContext(AppContext);
     return (
-        <div id="main">
+        <div id="main" className={theme === 'dark' ? 'dark-page' : 'light-page'}>
           <Header/>
           <Body/>
           <Footer/>
