fbb51ee6acafe37d41454149b3f9fbbcd1a9257d
BrunoDevMoraes <brunodevmoraes@gmail.com>
2021-11-07 13:30:52 -0300

    Fix translate bug

--- src/data.js
+++ src/data.js
@@ -25,9 +25,9 @@ export const certifications = {
     {img: Trybe, name: 'Web Development Fundamentals', issuance: 'Issuance: 07/2021', expiration: 'Expiration: No expiration', link: 'https://v2.credential.net/0a53b87f-7162-4084-9884-4a641c171862#gs.e5xeh5'},
   ],
   PT: [
-    {img: Trybe, name: 'Desenvolvimento Front-End', issuance: 'Emissão: 10/2021', expiration: 'Expiração: Sem expiration', link: 'https://v2.credential.net/64fb8441-1b9a-4c44-85dc-51b3639d14ed#gs.f49lp4'},
-    {img: WiseUp, name: 'Curso de Inglês Wise Up', issuance: 'Emissão: 10/2021', expiration: 'Expiração: Sem expiration', link: 'https://bit.ly/3lYVYJp'},
-    {img: Trybe, name: 'Fundamentos do Desenvolvimento Web', issuance: 'Emissão: 07/2021', expiration: 'Expiração: Sem expiration', link: 'https://v2.credential.net/0a53b87f-7162-4084-9884-4a641c171862#gs.e5xeh5'},
+    {img: Trybe, name: 'Desenvolvimento Front-End', issuance: 'Emissão: 10/2021', expiration: 'Expiração: Sem expiração', link: 'https://v2.credential.net/64fb8441-1b9a-4c44-85dc-51b3639d14ed#gs.f49lp4'},
+    {img: WiseUp, name: 'Curso de Inglês Wise Up', issuance: 'Emissão: 10/2021', expiration: 'Expiração: Sem expiração', link: 'https://bit.ly/3lYVYJp'},
+    {img: Trybe, name: 'Fundamentos do Desenvolvimento Web', issuance: 'Emissão: 07/2021', expiration: 'Expiração: Sem expiração', link: 'https://v2.credential.net/0a53b87f-7162-4084-9884-4a641c171862#gs.e5xeh5'},
   ]
 }
 
