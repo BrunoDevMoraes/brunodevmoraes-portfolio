c18ae221dbaace5d824c1e457d2aa35c61460433
BrunoDevMoraes <brunodevmoraes@gmail.com>
2021-11-08 00:40:20 -0300

    Add hidden menu style to english version

--- src/components/HiddenMenu/HiddenMenu.jsx
+++ src/components/HiddenMenu/HiddenMenu.jsx
@@ -13,11 +13,20 @@ const { language, setHiddenMenu, hiddenMenu, theme } = useContext(AppContext);
 
   if (language === 'PT') {
     return (
-      <div id="hidden-menu">
-        <h4>Sobre mim</h4>
-        <h4>Habilidades</h4>
-        <h4>Projetos</h4>
-        <h4>Certificações</h4>
+      <div id="hidden-menu" className={theme === 'dark' ? 'dark-hidden-menu' : 'light-hidden-menu'}>
+        <a href="#about" onClick={handleHiddenClick}>
+        <div className={theme === 'dark' ? 'dark-option' : 'light-option'}><h4>Sobre mim</h4></div>
+        </a>
+        <a href="#abilities" className={theme === 'dark' ? 'dark-option' : 'light-option'} onClick={handleHiddenClick}>
+          <div className={theme === 'dark' ? 'dark-option' : 'light-option'}><h4>Habilidades</h4></div>
+        </a>
+        <a href="#projects-section" className={theme === 'dark' ? 'dark-option' : 'light-option'} onClick={handleHiddenClick}>
+          <div className={theme === 'dark' ? 'dark-option' : 'light-option'}><h4>Projetos</h4></div>
+        </a>
+        <a href="#certifications-full-section" className={theme === 'dark' ? 'dark-option' : 'light-option'} onClick={handleHiddenClick}>
+          <div className={theme === 'dark' ? 'dark-option' : 'light-option'}><h4>Certificações</h4></div>
+        </a>
+        <div id="decorative-rectangle" className={theme === 'dark' ? 'dark-decorative-rectangle' : 'light-decorative-rectangle'}/>
       </div>
     );
   }
